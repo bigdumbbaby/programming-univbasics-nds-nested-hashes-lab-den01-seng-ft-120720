7afb53aeb21f6059ad854d52fdee5f36869523a9
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-26 22:31:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,6 +18,8 @@ def literal_aoh
   # held in the constants
   
   literal_array_of_hashes = [
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Pat Monteseian", occupation: "Staff" },
     { name:  "Kate Gompert", occupation: "None" },
     { name:  "Bruce Green", occupation: "Fan of Mildred" }]
